9672d42ce57fd21181e1690ae9cb91fb644e6373
Yisen Jing <jyisen@hotmail.com>
2016-05-11 14:32:40 -0700

    learn to store image in local iphone

--- Storing Images/ViewController.swift
+++ Storing Images/ViewController.swift
@@ -10,9 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Eisen1.jpg/500px-Eisen1.jpg")
+        
+        let urlRequest = NSURLRequest(URL: url!)
+        
+        NSURLConnection.sendAsynchronousRequest(urlRequest, queue: NSOperationQueue.mainQueue(), completionHandler: {
+            response, data, error in
+            
+            if error != nil {
+                println("There was an error!")
+                
+                
+            } else {
+                let image = UIImage(data: data)
+                
+                //self.imageView.image = image
+                
+                var documentsDirectory: String?
+                
+                var paths:[AnyObject] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+                
+                if paths.count > 0 {
+                    
+                    documentsDirectory = paths[0] as? String
+                    
+                    var savePath = documentsDirectory! + "/japan.jpg"
+                    
+                    NSFileManager.defaultManager().createFileAtPath(savePath, contents: data, attributes: nil)
+                    
+                    
+                    self.imageView.image = UIImage(named: savePath)
+                    
+                    
+                    
+                    
+                }
+                
+                
+                
+            }
+            
+            }
+        
+        )
+        
     }
 
     override func didReceiveMemoryWarning() {
